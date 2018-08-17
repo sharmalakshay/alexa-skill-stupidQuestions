e5d05797c0df8522396ae5128a15dcddc534969d
Lakshay Sharma <lsharma@tcd.ie>
2018-08-18 00:13:25 +0100

    Adding some speech synthesis

--- node-modules/index.js
+++ node-modules/index.js
@@ -14,7 +14,9 @@ const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Cool!';
 
 const data = [
-    'A stupid question',
+    'Why do people not believe it when someone say earth is flat?',
+    'Why is it that there is no "W" in <emphasis level="strong">"one"</emphasis>,<break time="0.5s"/> but there\'s a "W" in <emphasis level="strong">"Two"</emphasis><break time="0.5s"/><prosody rate="x-slow"> and </prosody><break time="0.05s"/> we don\'t use it?',
+    '<prosody rate="slow">Can I trust you to be a real person?<break time="0.5s"/></prosody> You could be a fake bot on the internet. <break time="0.1s"/> <prosody rate="slow" volume="x-soft" pitch="low"><emphasis level="strong">And I have standards.</emphasis></prosody>'
 ];
 
 const handlers = {
