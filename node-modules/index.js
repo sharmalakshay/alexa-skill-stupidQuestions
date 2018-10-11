23136a7f3d810b4d0837b1bf739e3027a3a97e8f
Lakshay Sharma <lsharma@tcd.ie>
2018-10-11 11:56:44 +0100

    live code

--- node-modules/index.js
+++ node-modules/index.js
@@ -5,7 +5,7 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 
-const APP_ID = 'amzn1.ask.skill.058eae46-3dd7-49f0-898a-bbd9e3d83b1e';
+const APP_ID = 'nope_aint_gonna_tell_you';
 
 const SKILL_NAME = 'Stupid Questions';
 const GET_QUESTION_MESSAGE = "";
@@ -14,9 +14,29 @@ const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Cool!';
 
 const data = [
-    'Why do people not believe it when someone say earth is flat?',
+    'Why do people not believe it when someone say that earth is flat?',
     'Why is it that there is no "W" in "one", but there\'s a "W" in "Two" and we don\'t use it?',
-    'Can I trust you to be a real person? You could be a fake bot on the internet. And I have standards.'
+    'Can I trust you to be a real person? You could be a fake bot on the internet. And I have standards.',
+    'Why is an electrical outlet called an outlet when you plug things into it? Shouldn\'t it be called an inlet?',
+    'Why does someone believe you when you say there are four billion stars, but check when you say the paint is wet?',
+    'Did they purposely make dyslexia hard to spell?',
+    'Why do we call them oranges when half of \’em are yellow?',
+    'Why is it that when you\'re driving and looking for an address, you turn down the volume on the radio?',
+    'Why can\'t women put on mascara with their mouth closed?',
+    'Why isn\'t there a mouse-flavored cat food?',
+    'Why is \“phonics\” not spelled the way it sounds?',
+    'Why isn\’t 11 pronounced \“onety-one\”?',
+    'If a word is misspelled in the dictionary, how would we ever know?',
+    'What do people in China call their good plates?',
+    'How important does a person have to be before they are considered assassinated instead of just murdered?',
+    'Why are Softballs hard?',
+    'If you mated a bull dog and a shitsu, would it be called a bullshit?',
+    'Why is it that night falls but day breaks?',
+    'If electricity comes from electrons, does morality come from morons?',
+    'What do you call it when fat people swim naked?',
+    'Why does your gynecologist leave the room when you undress?',
+    'How do \“Do not walk on the grass\” signs get there?',
+    'Why is the word for “a fear of long words,\” hippopotomonstrosesquippedaliophobia, so long?'
 ];
 
 const handlers = {
